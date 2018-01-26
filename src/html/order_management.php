604bb24010d283299c954de9e3123d7bf812aac3
Sara <sara.kiade@studio.unibo.it>
2018-01-27 00:41:00 +0100

    fatta pagina ordini lato user, mancano dettagli

--- src/html/order_management.php
+++ src/html/order_management.php
@@ -47,19 +47,164 @@
   </header>
 
   <section>
+
     <div class="tab">
       <button class="tablinks" onclick="openList(event, 'current')">Ordini in corso</button>
       <button class="tablinks" onclick="openList(event, 'previous')">Ordini precedenti</button>
     </div>
 
     <div id="current" class="tabcontent">
-      <?php
-        for ($i=0; $i <10; $i++) {
-          print <<< END
-          <p>$i</p>
-END;
-        }
-      ?>
+      <div class="row col-titles">
+        <div class="col-sm-3 field-title">id ordine</div>
+        <div class="col-sm-4 field-title">riepilogo</div>
+        <div class="col-sm-3 field-title">stato</div>
+        <div class="col-sm-2 field-title"></div>
+
+      </div>
+      <div class="ord-body">
+        <div class="row order">
+          <div class="col-sm-3 ord-info id-ord">
+            <span>
+              <h1>123</h1>
+            </span>
+          </div>
+
+
+          <div class="col-sm-4 ord-info">
+            <div class="tl-row">
+              <div class="timeline">
+                    <span class="stat">inattivo</span>
+                      <span class="stat">pendente</span>
+                      <span class="stat">partito</span>
+                      <span class="stat stat-active">concluso</span>
+              </div>
+            </div>
+          </div>
+
+            <div class="col-sm-3 ord-info riep">
+                <div class="riepilogo">
+                  <span class="field">
+                    <p class="field-name">Indirizzo: </p>
+                    <p class="field-value">Via 'ndante </p>
+                  </span>
+                  <span class="field">
+                    <p class="field-name">Campanello: </p>
+                    <p class="field-value">ciccio pasticcio</p>
+                  </span>
+                  <span class="field">
+                    <p class="field-name">Data: </p>
+                    <p class="field-value">10/02/2018</p>
+                  </span>
+                  <span class="field">
+                    <p class="field-name">Ora: </p>
+                    <p class="field-value">12:35</p>
+                  </span>
+                </div>
+          </div>
+
+
+          <div class="col-sm-2 ord-info trash">
+            <button type="button" class="btn btn-link btn-lg">
+									<span class="glyphicon glyphicon-trash"> </span>
+						</button>
+          </div>
+      </div>
+
+      <div class="row order">
+        <div class="col-sm-3 ord-info id-ord">
+          <span>
+            <h1>333</h1>
+          </span>
+        </div>
+
+
+        <div class="col-sm-4 ord-info">
+          <div class="tl-row">
+            <div class="timeline">
+                  <span class="stat">inattivo</span>
+                    <span class="stat">pendente</span>
+                    <span class="stat">partito</span>
+                    <span class="stat stat-active">concluso</span>
+            </div>
+          </div>
+        </div>
+
+          <div class="col-sm-3 ord-info riep">
+              <div class="riepilogo">
+                <span class="field">
+                  <p class="field-name">Indirizzo: </p>
+                  <p class="field-value">Via roma, 56 </p>
+                </span>
+                <span class="field">
+                  <p class="field-name">Campanello: </p>
+                  <p class="field-value">muzio scevola</p>
+                </span>
+                <span class="field">
+                  <p class="field-name">Data: </p>
+                  <p class="field-value">01/02/2018</p>
+                </span>
+                <span class="field">
+                  <p class="field-name">Ora: </p>
+                  <p class="field-value">08:40</p>
+                </span>
+              </div>
+        </div>
+
+
+        <div class="col-sm-2 ord-info trash">
+          <button type="button" class="btn btn-link btn-lg">
+                <span class="glyphicon glyphicon-trash"> </span>
+          </button>
+        </div>
+    </div>
+
+    <div class="row order">
+      <div class="col-sm-3 ord-info id-ord">
+        <span>
+          <h1>274</h1>
+        </span>
+      </div>
+
+
+      <div class="col-sm-4 ord-info">
+        <div class="tl-row">
+          <div class="timeline">
+                <span class="stat">inattivo</span>
+                  <span class="stat">pendente</span>
+                  <span class="stat">partito</span>
+                  <span class="stat stat-active">concluso</span>
+          </div>
+        </div>
+      </div>
+
+        <div class="col-sm-3 ord-info riep">
+            <div class="riepilogo">
+              <span class="field">
+                <p class="field-name">Indirizzo: </p>
+                <p class="field-value">Via delle rose </p>
+              </span>
+              <span class="field">
+                <p class="field-name">Campanello: </p>
+                <p class="field-value">lina carto</p>
+              </span>
+              <span class="field">
+                <p class="field-name">Data: </p>
+                <p class="field-value">3/02/2018</p>
+              </span>
+              <span class="field">
+                <p class="field-name">Ora: </p>
+                <p class="field-value">13:50</p>
+              </span>
+            </div>
+      </div>
+
+
+      <div class="col-sm-2 ord-info trash">
+        <button type="button" class="btn btn-link btn-lg">
+              <span class="glyphicon glyphicon-trash"> </span>
+        </button>
+      </div>
+  </div>
     </div>
 
     <div id="previous" class="tabcontent">
@@ -68,19 +213,28 @@ END;
   </section>
 
   <script>
-    function openList(evt, category) {
-      var i, tabcontent, tablinks;
-      tabcontent = document.getElementsByClassName("tabcontent");
-      for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-      }
-      tablinks = document.getElementsByClassName("tablinks");
-      for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-      }
-      document.getElementById(category).style.display = "block";
-      evt.currentTarget.className += " active";
+  function openList(evt, category) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
     }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(category).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  </script>
+
+  <script type="text/javascript">
+  $(function() {
+  $('.timeline').on('click', '.stat', function() {
+      $('.stat').removeClass('stat-active');
+      $(this).addClass('stat-active');
+  })
+})
   </script>
 </body>
 </html>
