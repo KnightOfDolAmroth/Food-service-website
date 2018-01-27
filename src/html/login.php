f96f6a556597303c0828639b15d4e0e3a7776e56
The Sav <filippo.savini2@studio.unibo.it>
2018-01-27 16:32:58 +0100

    First stylistic fixes to login and registration modals

--- src/html/login.php
+++ src/html/login.php
@@ -19,13 +19,13 @@ if(isset($_COOKIE["username"]) && ($_COOKIE["password"] != 'false')) {
 			WHERE username = '$usr'";
 	$result = $conn->query($sql0) or trigger_error($conn->error."[$sql0]");
 	$row = $result->fetch_assoc();
-	
+
 	if ($row["password"] === $_COOKIE["password"]) {
 		$conn->close();
 		$_SESSION["username"] = $_COOKIE["username"];
 		$_SESSION["password"] = $_COOKIE["password"];
 		header('Location: ./user_home.php');
-	}	
+	}
 }
 ?>
 
@@ -34,12 +34,14 @@ if(isset($_COOKIE["username"]) && ($_COOKIE["password"] != 'false')) {
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
  <link href="../css/login.css" rel="stylesheet" type="text/css"/>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  <script src="../js/login.js" type="text/javascript"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
 
 <div id="log_form" class="modal">
-  
+
   <form class="modal-content animate" action="./login.php" method="post">
     <div class="container">
       <label for="uname"><b>Username</b></label>
@@ -47,42 +49,42 @@ if(isset($_COOKIE["username"]) && ($_COOKIE["password"] != 'false')) {
 
       <label for="pwd"><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="pwd" id="pwd" required>
-        
-      <button type="submit" id="accedi1">Accedi</button>
-	  <button type="reset" id="registrati" onclick="registration()" >Registrati</button>
-      <label>
+
+      <button type="submit" class="btn btn-primary btn-lg btn-block btn-huge" id="accedi1">Accedi</button>
+	  	<button type="reset" class="btn btn-primary btn-lg btn-block btn-huge" id="registrati" onclick="registration()">Registrati</button>
+      <label style="font-size: 12px;">
         <input type="checkbox" checked="checked" name="rememberme" id="rememberme" value="1" > Remember me
       </label>
     </div>
 
     <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="window.location.href='./homepage.html'" class="cancelbtn">Cancel</button>
+      <button type="button" onclick="window.location.href='./homepage.html'" class="btn btn-danger">Cancel</button>
       <span class="psw">Forgot <a onclick="forgot_pwd()" style="cursor: pointer; cursor: hand;">password?</a></span>
     </div>
   </form>
 </div>
 
 <div id="reg_form" class="modal">
-  
-  <form class="modal-content animate" method="post">
+
+  <form class="modal-content animate" id="registrazione" method="post">
     <div class="container">
 	  <label for="new_mail"><b>Mail</b></label>
       <input type="email" placeholder="Enter mail" name="new_mail" id="new_mail" required>
-	  
+
 	  <label for="new_telephone"><b>Telefono</b></label>
       <input type="text" placeholder="Enter telephone" name="new_telephone" id="new_telephone" required>
-	  
+
       <label for="new_uname"><b>Username</b></label>
       <input type="text" placeholder="Enter Username" name="new_uname" id="new_uname" required>
 
       <label for="new_psw"><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="new_psw" id="new_psw" required>
-	  
+
 	  <label for="new_rep_psw"><b>Repeat Password</b></label>
       <input type="password" placeholder="Enter Password" name="new_rep_psw" id="new_rep_psw" required>
-     
-      <button type="submit" id="accedi2">Submit</button>
-	  <button type="button" onclick="window.location.href='./login.php'" id="cancel">Cancel</button>
+
+      <button type="submit" class="btn btn-primary btn-lg btn-block btn-huge" id="accedi2">Submit</button>
+	  <button type="button" class="btn btn-primary btn-lg btn-block btn-huge" onclick="window.location.href='./login.php'" id="cancel">Cancel</button>
       <label>
         <input type="checkbox" checked="checked"> Remember me
 		<p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
@@ -92,12 +94,12 @@ if(isset($_COOKIE["username"]) && ($_COOKIE["password"] != 'false')) {
 </div>
 
 <div id="forgot_pwd_form" class="modal">
-  
+
   <form class="modal-content animate" action="./login.php" method="post">
     <div class="container">
       <label for="mail_fp"><b>Mail</b></label>
       <input type="email" placeholder="Enter mail" name="mail_fp" id="mail_fp" required>
-        
+
       <button type="submit" id="accedi3">Invia</button>
     </div>
 
@@ -135,28 +137,28 @@ if(isset($_COOKIE["username"]) && ($_COOKIE["password"] != 'false')) {
 			}
 		}
 	?>
-	
+
 <?php
 		if (isset($_REQUEST["new_mail"]) && isset($_REQUEST["new_telephone"]) && isset($_REQUEST["new_uname"])
 			&& isset($_REQUEST["new_psw"]) && isset($_REQUEST["new_rep_psw"])) {
-			
+
 			if ($_REQUEST["new_psw"] === $_REQUEST["new_rep_psw"]) {
 				$username = $_REQUEST["new_uname"];
 				$email = $_REQUEST["new_mail"];
-			
+
 				$sql1 = "SELECT *
 					FROM utente
 					WHERE username = '$username'
 					OR email = '$email'";
 				$result = $conn->query($sql1) or trigger_error($conn->error."[$sql1]");
-				
+
 				if($result->num_rows === 0) {
 					$telephone = $_REQUEST["new_telephone"];
 					$punti = 0;
 					$salt = substr(md5(microtime()),rand(0,26),32);
 					$password = $_REQUEST["new_psw"].$salt;
 					$password = sha1($password);
-					
+
 					$sql2 = "INSERT INTO utente(username, password, email, telefono, punti, salt)
 						VALUES ('$username', '$password', '$email', '$telephone', '$punti', '$salt')";
 					$conn->query($sql2) or trigger_error($conn->error."[$sql2]");
@@ -170,7 +172,7 @@ if(isset($_COOKIE["username"]) && ($_COOKIE["password"] != 'false')) {
 			}
 		}
 	?>
-	
+
 <?php
 		if (isset($_REQUEST["mail_fp"])) {
 			echo "LETTURA DB";
@@ -182,7 +184,7 @@ if(isset($_COOKIE["username"]) && ($_COOKIE["password"] != 'false')) {
 			$row = $result->fetch_assoc();
 			echo "DB LETTO";
 			if ($row["email"] === $email) {
-				$new_pwd = substr(md5(microtime()),rand(0,26),10);				
+				$new_pwd = substr(md5(microtime()),rand(0,26),10);
 				$subject = "Your Recovered Password";
 				$message = "Please use this new password to login: ".$new_pwd;
 				$headers = "From: prova@unibo.it";
@@ -203,6 +205,6 @@ if(isset($_COOKIE["username"]) && ($_COOKIE["password"] != 'false')) {
 			$conn->close();
 		}
 	?>
-	
+
 </body>
 </html>
