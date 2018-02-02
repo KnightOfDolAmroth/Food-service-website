66bf379c01a6817646ce11f0bb01deed1e68f577
MATTEO MAGNINI <matteo.magnini@studio.unibo.it>
2018-02-02 14:36:50 +0100

    menu selection glitch solved

--- src/html/user/menu.php
+++ src/html/user/menu.php
@@ -10,8 +10,6 @@ if ($conn->connect_error) {
 }
 
 session_start();
-//$_SESSION['username']=$_SESSION['username'];
-//include 'add_preferiti.php';
 ?>
 
 <!DOCTYPE html>
@@ -76,296 +74,291 @@ session_start();
 					}  
 				});
 				setTimeout(function () { location.reload(true); }, 500);
-			});  
+			});
+
+			$('.selector').click(function(){  
+				var id_selezione = $(this).attr("name");  
+				$.ajax({
+					url:"selezione.php",  
+					method:"post",  
+					data:{id_selezione:id_selezione},  
+					success:function(data){
+						console.log(id_selezione); 
+					}  
+				});				
+			});
 		});
-		
 		
 	</script>
   
-    <!--navbar in cima-->
     <nav class="navbar fixed-top navbar-inverse">
-      <div class="container-fluid">
+    <div class="container-fluid">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="../homepage/home.html">La Malaghiotta</a>
+			</button>
+			<a class="navbar-brand" href="../homepage/home.html">La Malaghiotta</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav navbar-nav">
+			<ul class="nav navbar-nav">
             <li><a href="#"><span class="glyphicon glyphicon-user"></span> Dati utente</a></li>
             <li><a href="#"><span class="glyphicon glyphicon-envelope"></span> Messaggi</a></li>
-          </ul>
-          <form class="navbar-form navbar-left" action="/action_page.php">
+			</ul>
+			<form class="navbar-form navbar-left" action="/action_page.php">
             <div class="input-group">
-              <input type="text" class="form-control" placeholder="Search">
-              <div class="input-group-btn">
-                <button class="btn btn-default" type="submit">
-                  <i class="glyphicon glyphicon-search"></i>
-                </button>
-              </div>
+				<input type="text" class="form-control" placeholder="Search">
+				<div class="input-group-btn">
+					<button class="btn btn-default" type="submit">
+					<i class="glyphicon glyphicon-search"></i>
+					</button>
+				</div>
             </div>
-          </form>
-          <ul class="nav navbar-nav navbar-right">
+			</form>
+			<ul class="nav navbar-nav navbar-right">
             <li><a href="../homepage/logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
-          </ul>
+			</ul>
         </div>
-      </div>
-    </nav> <!--fine navbar in cima-->
+    </div>
+    </nav>
+	
+	<div class="bkg">
+		<img class="img-responsive" src="../../../img/piadona.jpeg" alt="background piada">
+	</div>
 
-<div class="bkg">
-  <img class="img-responsive" src="../../../img/piadona.jpeg" alt="background piada">
-</div>
-  <!--tabs-->
-
-    <!-- Nav tabs -->
     <ul class="nav nav-tabs" role="tablist" id="tablist">
 
-      <li role="presentation" class="active">
-        <a  href="#tutto" aria-controls="tutto" role="tab" data-toggle="tab">
+      <li role="presentation" <?php if(isset($_SESSION['menu']) && $_SESSION['menu'] === 'tutto') {echo 'class="active"';}?>>
+        <a href="#tutto" aria-controls="tutto" role="tab" data-toggle="tab" name="tutto" class="selector">
             <img class="img-responsive img-circle tab-img" src="../../../img/logo.jpg" alt="tutti i prodotti">
         </a>
-
       </li>
 
-      <li role="presentation">
-        <a href="#piadine" aria-controls="piadine" role="tab" data-toggle="tab">
+      <li role="presentation" <?php if(isset($_SESSION['menu']) && $_SESSION['menu'] === 'piadine') {echo 'class="active"';}?>>
+        <a href="#piadine" aria-controls="piadine" role="tab" data-toggle="tab" name="piadine" class="selector">
           <img class="img-responsive img-circle tab-img" src="../../../img/piadine.jpg" alt="piadine">
         </a>
       </li>
 
-      <li role="presentation">
-        <a href="#crescioni" aria-controls="crescioni" role="tab" data-toggle="tab">
+      <li role="presentation" <?php if(isset($_SESSION['menu']) && $_SESSION['menu'] === 'crescioni') {echo 'class="active"';}?>>
+        <a href="#crescioni" aria-controls="crescioni" role="tab" data-toggle="tab" name="crescioni" class="selector">
           <img class="img-responsive img-circle tab-img" src="../../../img/crescione.jpg" alt="crescioni">
         </a>
       </li>
 
-      <li role="presentation">
-        <a href="#rotoli" aria-controls="rotoli" role="tab" data-toggle="tab">
+      <li role="presentation" <?php if(isset($_SESSION['menu']) && $_SESSION['menu'] === 'rotoli') {echo 'class="active"';}?>>
+        <a href="#rotoli" aria-controls="rotoli" role="tab" data-toggle="tab" name="rotoli" class="selector">
           <img class="img-responsive img-circle tab-img" src="../../../img/rotoli.jpg" alt="rotoli">
         </a>
       </li>
 
-      <li role="presentation">
-        <a href="#altro" aria-controls="altro" role="tab" data-toggle="tab">
+      <li role="presentation" <?php if(isset($_SESSION['menu']) && $_SESSION['menu'] === 'altro') {echo 'class="active"';}?>>
+        <a href="#altro" aria-controls="altro" role="tab" data-toggle="tab" name="altro" class="selector">
           <img class="img-responsive img-circle tab-img" src="../../../img/altro.jpg" alt="altro">
         </a>
       </li>
 
 
-      <li role="presentation">
-        <a href="#bibite" aria-controls="bibite" role="tab" data-toggle="tab">
+      <li role="presentation" <?php if(isset($_SESSION['menu']) && $_SESSION['menu'] === 'bibite') {echo 'class="active"';}?>>
+        <a href="#bibite" aria-controls="bibite" role="tab" data-toggle="tab" name="bibite" class="selector">
           <img class="img-responsive img-circle tab-img" src="../../../img/bibite.jpg" alt="bibite">
         </a>
       </li>
 
-			<li role="presentation">
-				<a href="#preferiti" aria-controls="preferiti" role="tab" data-toggle="tab">
-					<img class="img-responsive img-circle tab-img" src="../../../img/preferiti.png" alt="preferiti">
-				</a>
-			</li>
+	  <li role="presentation" <?php if(isset($_SESSION['menu']) && $_SESSION['menu'] === 'preferiti') {echo 'class="active"';}?>>
+		<a href="#preferiti" aria-controls="preferiti" role="tab" data-toggle="tab" name="preferiti" class="selector">
+			<img class="img-responsive img-circle tab-img" src="../../../img/preferiti.png" alt="preferiti">
+		</a>
+	  </li>
     </ul>
-
-    <!-- Tab panes -->
+	
     <div class="tab-content">
 
-      <div role="tabpanel" class="tab-pane active" id="tutto">
-          <div class="tab-elements">
-            <div class="title-element">
-              <h1>Tutti i nostri prodotti</h1>
-            </div>
+		<div role="tabpanel" class="tab-pane <?php if($_SESSION['menu'] === 'tutto') {echo 'active';}?>" id="tutto">
+			<div class="tab-elements">
+				<div class="title-element">
+				  <h1>Tutti i nostri prodotti</h1>
+				</div>
+				<div class="container" id=prod-container>
+					<div class="row">
+						<?php
+						$sql0 = " SELECT *
+								FROM prodotto";
+						$result = $conn->query($sql0) or trigger_error($conn->error."[$sql0]");
 
-            <div class="container" id=prod-container>
-                  <div class="row">
+						if ($result->num_rows > 0) {
+							while($row = $result->fetch_assoc()) {
+								include 'prodotti.php';
+							}
+						}
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
 
-				  <?php
+		<div role="tabpanel" class="tab-pane <?php if($_SESSION['menu'] === 'piadine') {echo 'active';}?>" id="piadine">
+			<div class="tab-elements">
+				<div class="title-element">
+					<h1>Le nostre piadine</h1>
+				</div>
+				<div class="container" id=prod-container>
+					<div class="row">
+						<?php
+						$sql1 = " SELECT *
+								FROM prodotto
+								WHERE tipo='Piadina'";
+						$result = $conn->query($sql1) or trigger_error($conn->error."[$sql1]");
+						if ($result->num_rows > 0) {
+							while($row = $result->fetch_assoc()) {
+								include 'prodotti.php';
+							}
+						}
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
 
-					$sql0 = " SELECT *
-							FROM prodotto";
-					$result = $conn->query($sql0) or trigger_error($conn->error."[$sql0]");
+		<div role="tabpanel" class="tab-pane <?php if($_SESSION['menu'] === 'crescioni') {echo 'active';}?>" id="crescioni">
+			<div class="tab-elements">
+				<div class="title-element">
+					<h1>I nostri crescioni</h1>
+				</div>
+				<div class="container" id=prod-container>
+					<div class="row">
+						<?php
+						$sql2 = " SELECT *
+								FROM prodotto
+								WHERE tipo='Crescione'";
+						$result = $conn->query($sql2) or trigger_error($conn->error."[$sql2]");
+						if ($result->num_rows > 0) {
+							while($row = $result->fetch_assoc()) {
+								include 'prodotti.php';
+							}
+						}
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
 
+		<div role="tabpanel" class="tab-pane <?php if($_SESSION['menu'] === 'rotoli') {echo 'active';}?>" id="rotoli">
+			<div class="tab-elements">
+				<div class="title-element">
+					<h1>I nostri rotoli</h1>
+				</div>
+				<div class="container" id=prod-container>
+					<div class="row">
+					<?php
+					$sql3 = " SELECT *
+							FROM prodotto
+							WHERE tipo='Rotolo'";
+					$result = $conn->query($sql3) or trigger_error($conn->error."[$sql3]");
 					if ($result->num_rows > 0) {
 						while($row = $result->fetch_assoc()) {
 							include 'prodotti.php';
 						}
 					}
-				  ?>
-                  </div>
-            </div>
-          </div>
-      </div>
-
-      <div role="tabpanel" class="tab-pane " id="piadine">
-        <div class="tab-elements">
-          <div class="title-element">
-            <h1>Le nostre piadine</h1>
-			</div>
-
-			<div class="container" id=prod-container>
-                  <div class="row">
-			
-			<?php
-				$sql1 = " SELECT *
-						FROM prodotto
-						WHERE tipo='Piadina'";
-				$result = $conn->query($sql1) or trigger_error($conn->error."[$sql1]");
-				if ($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()) {
-						include 'prodotti.php';
-					}
-				}
-				?>
-
-          </div>
-        </div>
-      </div>
-	</div>
-
-      <div role="tabpanel" class="tab-pane" id="crescioni">
-        <div class="tab-elements">
-          <div class="title-element">
-            <h1>I nostri crescioni</h1>
-			</div>
-			<div class="container" id=prod-container>
-                  <div class="row">
-			<?php
-				$sql2 = " SELECT *
-						FROM prodotto
-						WHERE tipo='Crescione'";
-				$result = $conn->query($sql2) or trigger_error($conn->error."[$sql2]");
-				if ($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()) {
-						include 'prodotti.php';
-					}
-				}
-				?>
-          </div>
-        </div>
-      </div>
-	  </div>
-
-      <div role="tabpanel" class="tab-pane" id="rotoli">
-        <div class="tab-elements">
-          <div class="title-element">
-            <h1>I nostri rotoli</h1>
-			</div>
-			<div class="container" id=prod-container>
-                  <div class="row">
-			<?php
-				$sql3 = " SELECT *
-						FROM prodotto
-						WHERE tipo='Rotolo'";
-				$result = $conn->query($sql3) or trigger_error($conn->error."[$sql3]");
-				if ($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()) {
-						include 'prodotti.php';
-					}
-				}
-				?>
-          </div>
-        </div>
-		</div>
-      </div>
-      <div role="tabpanel" class="tab-pane" id="altro">
-        <div class="tab-elements">
-          <div class="title-element">
-            <h1>Altre specialità</h1>
-			</div>
-			<div class="container" id=prod-container>
-                  <div class="row">
-			<?php
-				$sql4 = " SELECT *
-						FROM prodotto
-						WHERE tipo='Altro'";
-				$result = $conn->query($sql4) or trigger_error($conn->error."[$sql4]");
-				if ($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()) {
-						include 'prodotti.php';
-					}
-				}
-				?>
-          </div>
-        </div>
-      </div>
-	  </div>
-	  
-      <div role="tabpanel" class="tab-pane" id="bibite">
-        <div class="tab-elements">
-          <div class="title-element">
-            <h1>Le nostre bibite</h1>
-			</div>
-			<div class="container" id=prod-container>
-                  <div class="row">
-			<?php
-				$sql5 = " SELECT *
-						FROM prodotto
-						WHERE tipo='Bibite'";
-				$result = $conn->query($sql5) or trigger_error($conn->error."[$sql5]");
-				if ($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()) {
-						include 'prodotti.php';
-					}
-				}
-				?>
-          </div>
-		  </div>
-        </div>
-      </div>
-
-
-			<div role="tabpanel" class="tab-pane" id="preferiti">
-				<div class="tab-elements">
-					<div class="title-element">
-						<h1>I tuoi preferiti</h1>
-						</div>
-						<div class="container" id=prod-container>
-                  <div class="row">
-			<?php
-				$user= $_SESSION['username'];
-				$sql6 = "SELECT *
-						FROM prodotto
-						WHERE id_prodotto IN (
-							SELECT id_prodotto
-							FROM preferisce
-							WHERE username='$user'
-						)";
-				$result = $conn->query($sql6) or trigger_error($conn->error."[$sql6]");
-				if ($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()) {
-						include 'prodotti.php';
-					}
-				}
-				?>
-				</div>
+					?>
 					</div>
 				</div>
 			</div>
 		</div>
+		
+		<div role="tabpanel" class="tab-pane <?php if($_SESSION['menu'] === 'altro') {echo 'active';}?>" id="altro">
+			<div class="tab-elements">
+				<div class="title-element">
+					<h1>Altre specialità</h1>
+				</div>
+				<div class="container" id=prod-container>
+					<div class="row">
+					<?php
+					$sql4 = " SELECT *
+							FROM prodotto
+							WHERE tipo='Altro'";
+					$result = $conn->query($sql4) or trigger_error($conn->error."[$sql4]");
+					if ($result->num_rows > 0) {
+						while($row = $result->fetch_assoc()) {
+							include 'prodotti.php';
+						}
+					}
+					?>
+					</div>
+				</div>
+			</div>
+		</div>
+	  
+		<div role="tabpanel" class="tab-pane <?php if($_SESSION['menu'] === 'bibite') {echo 'active';}?>" id="bibite">
+			<div class="tab-elements">
+				<div class="title-element">
+					<h1>Le nostre bibite</h1>
+				</div>
+				<div class="container" id=prod-container>
+					<div class="row">
+					<?php
+					$sql5 = " SELECT *
+							FROM prodotto
+							WHERE tipo='Bibite'";
+					$result = $conn->query($sql5) or trigger_error($conn->error."[$sql5]");
+					if ($result->num_rows > 0) {
+						while($row = $result->fetch_assoc()) {
+							include 'prodotti.php';
+						}
+					}
+					?>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div role="tabpanel" class="tab-pane <?php if($_SESSION['menu'] === 'preferiti') {echo 'active';}?>" id="preferiti">
+			<div class="tab-elements">
+				<div class="title-element">
+					<h1>I tuoi preferiti</h1>
+				</div>
+				<div class="container" id=prod-container>
+                <div class="row">
+					<?php
+					$user= $_SESSION['username'];
+					$sql6 = "SELECT *
+							FROM prodotto
+							WHERE id_prodotto IN (
+								SELECT id_prodotto
+								FROM preferisce
+								WHERE username='$user'
+							)";
+					$result = $conn->query($sql6) or trigger_error($conn->error."[$sql6]");
+					if ($result->num_rows > 0) {
+						while($row = $result->fetch_assoc()) {
+							include 'prodotti.php';
+						}
+					}
+					?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 	
 	<div class="modal fade" id="data_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title" id="myModalLabel">Aggiungi al carrello</h4>
-        </div>
-        <div class="modal-body" id="dettagli_prodotto"></div>
-		<div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Annulla</button>
-			<button type="button" class="btn btn-primary" id="submit_form">Aggiungi
-              <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
-            </button>
-          </div>
-        </div>
-      </div>
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Aggiungi al carrello</h4>
+				</div>
+				<div class="modal-body" id="dettagli_prodotto"></div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Annulla</button>
+					<button type="button" class="btn btn-primary" id="submit_form">Aggiungi
+					  <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+		</div>
     </div>
 	
-	<?php
-		//include 'add_preferiti.php';
-		//include 'modalbis.php';
-		//include 'modal.php';
-	?>
 </body>
 </html>
