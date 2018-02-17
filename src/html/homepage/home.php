9e67145f1c60d495184186400657f9eb57572e9f
MATTEO MAGNINI <matteo.magnini@studio.unibo.it>
2018-02-17 15:38:15 +0100

    homepage adjustement

--- src/html/homepage/home.php
+++ src/html/homepage/home.php
@@ -121,5 +121,20 @@
     </div>
   </div>
 </div>
+
+<div class="modal fade" id="data_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Casella degli avvisi</h4>
+			</div>
+			<div class="modal-body" id="dettagli_messaggi"></div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Esci</button>
+			</div>
+		</div>
+	</div>
+</div>
 </body>
 </html>
